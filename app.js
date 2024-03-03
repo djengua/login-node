ef274a96cf459cf4c400c06916b81a632c1c126f
Bernardo Gabriel Mejía Pérez <bernardo.mejiaperez703@gmail.com>
2024-03-02 18:17:48 -0600

    Función login

--- app.js
+++ app.js
@@ -9,4 +9,17 @@ app.get("/hello-world", (req, res) => {
   res.send("Hello world!");
 });
 
+// ? Login
+const login = (user, pass) =>
+  (message = user == "Admin" && pass == "Password@123" ? "Correct" : "Error");
+
+// console.log(login("Admin", "Password@123"));
+
+app.get("/login", (req, res) => {
+  const user = req.query.user,
+    password = req.query.password;
+
+  res.send(login(user, password));
+});
+
 module.exports = app;
